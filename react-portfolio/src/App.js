a1a362f86e31f174b3cfe0a719a4b9de57808a86
Harrison Thomas <hthomas146@utexas.edu>
2020-02-23 12:19:40 -0600

    Navbar now links up the different routes for the portfolio

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Blog from "./components/pages/Blog";
-import Contact from "./components/pages/Contact";
+import NavTabs from "./components/NavTabs/NavTabs";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <NavTabs />
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
       </div>
     </Router>
