6f279804b28b3dbb70f10c62f446bb8ebe1c9d96
Harrison Thomas <hthomas146@utexas.edu>
2020-05-12 18:58:06 -0500

    Cleaned some stuff up, working on basic styling and organization

--- react-portfolio/src/components/About/About.js
+++ react-portfolio/src/components/About/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./About.css"
 
 function About() {
     return (
         <div>
             <h1>About Me</h1>
             <h1>Harrison Thomas</h1>
+            <img src={require(`../../../src/me.jpg`)}></img>
             <p>
                 I am a web developer from Austin, Texas. I specialize in using the MERN stack to create mobile-first, aesthetically principled and functional apps. I bring a multidisciplinary approach to programming with a professional background in restaurant management and an education in English. I am seeking a positions where I can improve both my leadership and programming skills in a fast-paced and challenging work environment.
             </p>
@@ -13,8 +15,8 @@ function About() {
                 <li>JavaScript</li>
                 <li>HTML</li>
                 <li>CSS</li>
-                <li>jQuery</li>
                 <li>React</li>
+                <li>jQuery</li>
             </ul>
             <p>
                 My hobbies beyond work include reading, writing, watching films and fitness.
