67fb5ab644a4efc2994242c45abb0d67dfbd5d56
Harrison Thomas <hthomas146@utexas.edu>
2020-05-17 15:04:14 -0500

    Break for today. Downloaded the font awesome React component and am researching how to implement the icons I actually want.

--- react-portfolio/src/components/About/About.js
+++ react-portfolio/src/components/About/About.js
@@ -1,28 +1,39 @@
 import React from "react";
-import "./About.css"
+import "./About.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function About() {
     return (
         <div>
+
+            {/* Image, Professional Description */}
             <div className="row aboutRow">
                 <div className="col-md-4">
-                    <h1>Harrison Thomas</h1>
-                    <img src={require(`../../../src/images/justme.png`)}></img>
+                    <h1 className="myName">Harrison Thomas</h1>
+                    <img className="justMe" src={require(`../../../src/images/justme.png`)}></img>
                 </div>
-                <div className="col-md-8">
+                <div className="col-md-8 aboutMe">
                     <p>
-                        I am a web developer from Austin, Texas. I specialize in using the MERN stack to create mobile-first, aesthetically principled and functional apps. I bring a multidisciplinary approach to programming with a professional background in restaurant management and an education in English. I am seeking a positions where I can improve both my leadership and programming skills in a fast-paced and challenging work environment.
+                        I am a web developer from Austin, Texas. I specialize in using the MERN stack to create mobile-first, aesthetically principled and functional apps. I bring a multidisciplinary approach to programming with a professional background in restaurant management and an education in English. I am seeking a position where I can improve both my leadership and programming skills in a fast-paced and challenging work environment.
             </p>
                 </div>
             </div>
-            <h3>Technologies:</h3>
-            <ul>
+
+            {/* Technologies */}
+            <div className="row techRow">
+                <FontAwesomeIcon icon={"js-square"}></FontAwesomeIcon>
+            </div>
+            {/* Insert a div that has icons of all of the tech I use */}
+            {/* <ul>
                 <li>JavaScript</li>
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>React</li>
                 <li>jQuery</li>
-            </ul>
+            </ul> */}
+
+            {/* Personal */}
             <p>
                 My hobbies beyond work include reading, writing, watching films and fitness.
             </p>
